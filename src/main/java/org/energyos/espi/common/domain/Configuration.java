8374b9572d75d238981c558094dc72ac3f61f137
Ian Zabel <pair+izabel@pivotallabs.com>
2013-11-14 11:18:02 -0500

    ThirdParty now uses Common's Configuration class

--- src/main/java/org/energyos/espi/common/domain/Configuration.java
+++ src/main/java/org/energyos/espi/common/domain/Configuration.java
@@ -2,6 +2,12 @@ package org.energyos.espi.common.domain;
 
 public class Configuration {
     public static final String DATA_CUSTODIAN_ID = "data_custodian";
+
+    // TODO: this string needs to be set up from the appropriate config file variable.
+    public static final String THIRD_PARTY_BASE_URL = "http://services.greenbuttondata.org/ThirdParty";
+    public static final String THIRD_PARTY_CLIENT_ID = "third_party";
+    public static final String THIRD_PARTY_CLIENT_PASSWORD = "secret";
+    
     public static final String[] SCOPES = new String[]{
             "FB=4_5_15;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13",
             "FB=4_5_16;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13"
