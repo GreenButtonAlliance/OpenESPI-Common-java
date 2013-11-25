e7cbb60a9e479bc671e36d5fd5e53b1481e7d9d8
Ian Zabel and Joshua Clark <pair+izabel+jclark@pivotallabs.com>
2013-11-25 15:58:20 -0500

    Adds SubscriptionService#findEntriesByHashedId

--- src/main/java/org/energyos/espi/common/service/SubscriptionService.java
+++ src/main/java/org/energyos/espi/common/service/SubscriptionService.java
@@ -1,6 +1,7 @@
 package org.energyos.espi.common.service;
 
 import org.energyos.espi.common.domain.Subscription;
+import org.energyos.espi.common.utils.EntryTypeIterator;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 import java.util.List;
@@ -13,4 +14,6 @@ public interface SubscriptionService {
     void persist(Subscription subscription);
 
     Subscription findByHashedId(String hashedId);
+
+    EntryTypeIterator findEntriesByHashedId(String hashedId);
 }
