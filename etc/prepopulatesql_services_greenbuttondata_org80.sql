9d21929a65cf550233ed593175d068fb0db00ed9
MartyBurns <marty@hypertek.us>
2014-01-02 07:20:14 -0500

    separated initialize sql script into versions based on host and port

--- etc/prepopulatesql_services_greenbuttondata_org80.sql
+++ etc/prepopulatesql_services_greenbuttondata_org80.sql
@@ -21,7 +21,7 @@ INSERT INTO retail_customers (username, first_name, last_name, password, enabled
 
 
 
-INSERT INTO application_information  (id, uuid, clientId, thirdPartyApplicationName, thirdPartyScopeSelectionScreenURI, thirdPartyNotifyUri, redirectURI, clientSecret) VALUES (1,'550e8400-e29b-41d4-a716-4466554413a0', 'third_party', 'Third Party (localhost)', 'http://localhost:8080/ThirdParty/RetailCustomer/ScopeSelection', 'http://localhost:8080/ThirdParty/espi/1_1/Notification', 'http://localhost:8080/ThirdParty/espi/1_1/OAuthCallBack', 'secret');
+INSERT INTO application_information  (id, uuid, clientId, thirdPartyApplicationName, thirdPartyScopeSelectionScreenURI, thirdPartyNotifyUri, redirectURI, clientSecret) VALUES (1,'550e8400-e29b-41d4-a716-4466554413a0', 'third_party', 'Third Party (localhost)', 'http://services.greenbuttondata.org/ThirdParty/RetailCustomer/ScopeSelection', 'http://services.greenbuttondata.org/ThirdParty/espi/1_1/Notification', 'http://services.greenbuttondata.org/ThirdParty/espi/1_1/OAuthCallBack', 'secret');
 INSERT INTO application_information_scopes (application_information_id, scope) VALUES (1, 'FB=4_5_15;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13');
 INSERT INTO application_information_scopes (application_information_id, scope) VALUES (1, 'FB=4_5_16;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13');
 
