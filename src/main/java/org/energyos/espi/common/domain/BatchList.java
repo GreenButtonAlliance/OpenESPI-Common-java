37bf49073f2ce909015e5f34ed8ae4fec3509e86
John Teeter <john.teeter@energyos.org>
2015-04-04 10:31:10 -0600

    Added Copyright comment and formatted the file

--- src/main/java/org/energyos/espi/common/domain/BatchList.java
+++ src/main/java/org/energyos/espi/common/domain/BatchList.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013, 2014, 2015 EnergyOS.org
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package org.energyos.espi.common.domain;
 
 import java.util.ArrayList;
@@ -22,27 +38,27 @@ import org.hibernate.annotations.LazyCollectionOption;
 @Entity
 public class BatchList {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @XmlTransient
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@XmlTransient
+	private Long id;
 
-    @ElementCollection
-    @LazyCollection(LazyCollectionOption.FALSE)
-    @CollectionTable(name = "resources", joinColumns=@JoinColumn(name="id"))
-    @Column(name = "uri")
-    @XmlElement(name = "resources")
-    private List<String> resources = new ArrayList<>();
+	@ElementCollection
+	@LazyCollection(LazyCollectionOption.FALSE)
+	@CollectionTable(name = "resources", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "uri")
+	@XmlElement(name = "resources")
+	private List<String> resources = new ArrayList<>();
 
-    public List<String> getResources() {
-        return resources;
-    }
+	public List<String> getResources() {
+		return resources;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
