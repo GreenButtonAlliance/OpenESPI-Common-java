e3d1525de45b6f91ecfb2b0d49200afc56242b38
John Teeter <john.teeter@energyos.org>
2015-04-04 10:43:51 -0600

    Updated Copyright; Formatted File; Fixed a couple of spelling errors

--- src/main/java/org/energyos/espi/common/domain/IdentifiedObject.java
+++ src/main/java/org/energyos/espi/common/domain/IdentifiedObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013, 2014 EnergyOS.org
+ * Copyright 2013, 2014, 2015 EnergyOS.org
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@
 // Any modifications to this file will be lost upon recompilation of the source schema. 
 // Generated on: 2013.08.27 at 01:43:57 PM EDT 
 //
-
 
 package org.energyos.espi.common.domain;
 
@@ -47,14 +46,18 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.energyos.espi.common.models.atom.LinkType;
 
-
 /**
- * This is a root class to provide common naming attributes for all classes needing naming attributes
+ * This is a root class to provide common naming attributes for all classes
+ * needing naming attributes
  * <p/>
- * <p>Java class for IdentifiedObject complex type.
+ * <p>
+ * Java class for IdentifiedObject complex type.
  * <p/>
- * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
  * <p/>
+ * 
  * <pre>
  * &lt;complexType name="IdentifiedObject">
  *   &lt;complexContent>
@@ -70,229 +73,217 @@ import org.energyos.espi.common.models.atom.LinkType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "IdentifiedObject")
-@XmlSeeAlso({
-        MeterReading.class,
-        UsagePoint.class,
-        ElectricPowerUsageSummary.class,
-        TimeConfiguration.class,
-        ApplicationInformation.class,
-        Authorization.class,
-        Subscription.class,
-        ElectricPowerQualitySummary.class,
-        IntervalBlock.class,
-        ReadingType.class
-})
+@XmlSeeAlso({ MeterReading.class, UsagePoint.class,
+		ElectricPowerUsageSummary.class, TimeConfiguration.class,
+		ApplicationInformation.class, Authorization.class, Subscription.class,
+		ElectricPowerQualitySummary.class, IntervalBlock.class,
+		ReadingType.class })
 @MappedSuperclass
-public class IdentifiedObject extends Resource implements Linkable, Serializable {
-	
-    @XmlTransient
+public class IdentifiedObject extends Resource implements Linkable,
+		Serializable {
+
+	@XmlTransient
 	private static final long serialVersionUID = -5263186855332223773L;
 
 	public interface ResourceEnumClass {
-		   ResourceEnum getEnumType();
+		ResourceEnum getEnumType();
 	}
-	
-    @XmlTransient
-    protected String description;
 
-    @XmlTransient
-    @NotNull
-    protected String uuid;
+	@XmlTransient
+	protected String description;
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @XmlTransient
-    protected Long id;
+	@XmlTransient
+	@NotNull
+	protected String uuid;
 
-    @XmlTransient
-    protected GregorianCalendar updated = new GregorianCalendar();
-    @XmlTransient
-    protected GregorianCalendar published = new GregorianCalendar();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@XmlTransient
+	protected Long id;
 
-    @XmlTransient
-    @Embedded
-    @AttributeOverrides( {
-            @AttributeOverride(name="rel", column = @Column(name="up_link_rel") ),
-            @AttributeOverride(name="href", column = @Column(name="up_link_href") ),
-    } )
-    private LinkType upLink;
+	@XmlTransient
+	protected GregorianCalendar updated = new GregorianCalendar();
+	@XmlTransient
+	protected GregorianCalendar published = new GregorianCalendar();
 
-    @XmlTransient
-    @Embedded
-    @AttributeOverrides( {
-            @AttributeOverride(name="rel", column = @Column(name="self_link_rel") ),
-            @AttributeOverride(name="href", column = @Column(name="self_link_href") ),
-    } )
-    private LinkType selfLink;
-    
-    @XmlTransient
-    // an enum to support class dispatching across the library
-    //
-    public enum ResourceEnum {
-  	  RetailCustomer(0), UsagePoint(1), MeterReadingC(2), IntervalBlock(3),
-  	  TimeConfiguration(4), ElectricPowerQualitySummary(5), ElectricPowerUsageSummary(6),
-  	  ReadingType(7), Subscription(8), Authorization(9), ApplicationInformation(10);
+	@XmlTransient
+	@Embedded
+	@AttributeOverrides({
+			@AttributeOverride(name = "rel", column = @Column(name = "up_link_rel")),
+			@AttributeOverride(name = "href", column = @Column(name = "up_link_href")), })
+	private LinkType upLink;
 
-  	  private int value;
+	@XmlTransient
+	@Embedded
+	@AttributeOverrides({
+			@AttributeOverride(name = "rel", column = @Column(name = "self_link_rel")),
+			@AttributeOverride(name = "href", column = @Column(name = "self_link_href")), })
+	private LinkType selfLink;
 
-  	  private ResourceEnum(final int value) {
-  	    this.value = value;
-  	  }
+	@XmlTransient
+	// an enum to support class dispatching across the library
+	//
+	public enum ResourceEnum {
+		RetailCustomer(0), UsagePoint(1), MeterReadingC(2), IntervalBlock(3), TimeConfiguration(
+				4), ElectricPowerQualitySummary(5), ElectricPowerUsageSummary(6), ReadingType(
+				7), Subscription(8), Authorization(9), ApplicationInformation(
+				10);
 
-  	  public int getValue() {
-  	    return value;
-  	  }
-    }
-    	  
-    public Long getId() {
-        return id;
-    }
+		private int value;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+		private ResourceEnum(final int value) {
+			this.value = value;
+		}
 
-    /**
-     * Gets the value of the mrid property.
-     *
-     * @return
-     *     possible object is
-     *     {@link String }
-     *
-     */
-    public String getMRID() {
-        if (uuid == null)
-            return null;
-        return "urn:uuid:" + uuid;
-    }
+		public int getValue() {
+			return value;
+		}
+	}
 
-    /**
-     * Sets the value of the mrid property.
-     *
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *
-     */
-    public void setMRID(String value) {
-        this.uuid = value.replace("urn:uuid:", "").toUpperCase();
-    }
+	public Long getId() {
+		return id;
+	}
 
-    /**
-     * Gets the value of the description property.
-     *
-     * @return
-     *     possible object is
-     *     {@link String }
-     *
-     */
-    public String getDescription() {
-        return description;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    /**
-     * Sets the value of the description property.
-     *
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *
-     */
-    public void setDescription(String value) {
-        this.description = value;
-    }
+	/**
+	 * Gets the value of the mrid property.
+	 *
+	 * @return possible object is {@link String }
+	 *
+	 */
+	public String getMRID() {
+		if (uuid == null)
+			return null;
+		return "urn:uuid:" + uuid;
+	}
 
-    public GregorianCalendar getUpdated() {
-        return updated;
-    }
+	/**
+	 * Sets the value of the mrid property.
+	 *
+	 * @param value
+	 *            allowed object is {@link String }
+	 *
+	 */
+	public void setMRID(String value) {
+		this.uuid = value.replace("urn:uuid:", "").toUpperCase();
+	}
 
-    public void setUpdated(GregorianCalendar updated) {
-        this.updated = updated;
-    }
+	/**
+	 * Gets the value of the description property.
+	 *
+	 * @return possible object is {@link String }
+	 *
+	 */
+	public String getDescription() {
+		return description;
+	}
 
-    public GregorianCalendar getPublished() {
-        return published;
-    }
+	/**
+	 * Sets the value of the description property.
+	 *
+	 * @param value
+	 *            allowed object is {@link String }
+	 *
+	 */
+	public void setDescription(String value) {
+		this.description = value;
+	}
 
-    public void setPublished(GregorianCalendar published) {
-        this.published = published;
-    }
+	public GregorianCalendar getUpdated() {
+		return updated;
+	}
 
-    public void setUUID(UUID uuid) {
-        this.uuid = uuid.toString().toUpperCase();
-        // make sure there is a uplink and a selflink
-        // so marshalling works ...
-        getSelfLink();
-        getUpLink();
-    }
+	public void setUpdated(GregorianCalendar updated) {
+		this.updated = updated;
+	}
 
-    public UUID getUUID() {
-        if (uuid != null)
-            return UUID.fromString(uuid);
-        return null;
-    }
+	public GregorianCalendar getPublished() {
+		return published;
+	}
 
-    public String getHashedId() {
-        return "" + getUUID();
-    }
+	public void setPublished(GregorianCalendar published) {
+		this.published = published;
+	}
 
-    @Override
-    public void setUpResource(IdentifiedObject resource) {
-    }
+	public void setUUID(UUID uuid) {
+		this.uuid = uuid.toString().toUpperCase();
+		// make sure there is a up link and a self link
+		// so marshaling works ...
+		getSelfLink();
+		getUpLink();
+	}
 
-    @Override
-    public String getParentQuery() {
-        return null;
-    }
+	public UUID getUUID() {
+		if (uuid != null)
+			return UUID.fromString(uuid);
+		return null;
+	}
 
-    @Override
-    public String getAllRelatedQuery() {
-        return null;
-    }
+	public String getHashedId() {
+		return "" + getUUID();
+	}
 
-    @Override
-    public List<String> getRelatedLinkHrefs() {
-        List<String> hrefs = new ArrayList<>();
-        for(LinkType link : getRelatedLinks()) {
-            hrefs.add(link.getHref());
-        }
-        return hrefs;
-    }
+	@Override
+	public void setUpResource(IdentifiedObject resource) {
+	}
 
-    public List<LinkType> getRelatedLinks() {
-        return new ArrayList<>();
-    }
+	@Override
+	public String getParentQuery() {
+		return null;
+	}
 
-    public LinkType getUpLink() {
-    	if (upLink == null) {
-    		setUpLink(new LinkType());
-    		upLink.setRel("up");
-    	}
-        return upLink;
-    }
+	@Override
+	public String getAllRelatedQuery() {
+		return null;
+	}
 
-    public void setUpLink(LinkType upLink) {
-        this.upLink = upLink;
-    }
+	@Override
+	public List<String> getRelatedLinkHrefs() {
+		List<String> hrefs = new ArrayList<>();
+		for (LinkType link : getRelatedLinks()) {
+			hrefs.add(link.getHref());
+		}
+		return hrefs;
+	}
 
-    public void merge(IdentifiedObject resource) {
-        this.setSelfLink(resource.getSelfLink());
-        this.setUpLink(resource.getUpLink());
-        this.setDescription(resource.getDescription());
-        this.setUpdated(resource.getUpdated());
-        this.setPublished(resource.getPublished());
-    }
+	public List<LinkType> getRelatedLinks() {
+		return new ArrayList<>();
+	}
 
-    public LinkType getSelfLink() {
-    	if (selfLink == null) {
-    		setSelfLink(new LinkType());
-    		selfLink.setRel("self");
-    	}
-        return selfLink;
-    }
+	public LinkType getUpLink() {
+		if (upLink == null) {
+			setUpLink(new LinkType());
+			upLink.setRel("up");
+		}
+		return upLink;
+	}
 
-    public void setSelfLink(LinkType selfLink) {
-        this.selfLink = selfLink;
-    }
+	public void setUpLink(LinkType upLink) {
+		this.upLink = upLink;
+	}
+
+	public void merge(IdentifiedObject resource) {
+		this.setSelfLink(resource.getSelfLink());
+		this.setUpLink(resource.getUpLink());
+		this.setDescription(resource.getDescription());
+		this.setUpdated(resource.getUpdated());
+		this.setPublished(resource.getPublished());
+	}
+
+	public LinkType getSelfLink() {
+		if (selfLink == null) {
+			setSelfLink(new LinkType());
+			selfLink.setRel("self");
+		}
+		return selfLink;
+	}
+
+	public void setSelfLink(LinkType selfLink) {
+		this.selfLink = selfLink;
+	}
 
 	public void unlink() {
 		// there is really nothing to unlink here
