1817ad9b5941add5b9a29fbb00e915ddae305639
Donald Coffin <donald.coffin@reminetworks.com>
2016-09-07 16:53:14 +0000

    Add GBA AWS Sandbox initialization files

---
+++ etc/initialize_GBA_AWS_Sandbox_databases.sh
@@ -0,0 +1,20 @@
+# This script will initialize the databases
+# it will first delete datacustodian and third party databases
+# then it creates them
+# then establishes all the tables
+# then populates tables with initial data
+#
+# Copy the appropriate version of prepopulatesql_<dc or tp>_<host><port>.sql to prepopulatesql.sql for your installation
+
+# drop/create databases 
+mysql --user=root --password=password < cleandatabases_dc.sql
+#mysql --user=root --password=password < cleandatabases_tp.sql
+
+# create tables 
+#mysql --user=root --password=password < thirdpartymysql.sql
+mysql --user=root --password=password < datacustodianmysql.sql
+mysql --user=root --password=password < tokenstore.sql
+
+# prepopulate tables 
+mysql --user=root --password=password < prepopulatesql_dc.sql
+#mysql --user=root --password=password < prepopulatesql_tp.sql
